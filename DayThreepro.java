e77623abd585dd1fd8ac0dfb54334e0cf04c29d8
Admin <sandhyabagale10@gmail.com>
2021-09-12 12:02:05 +0530

    we adding the length of the program

--- DayThreepro.java
+++ DayThreepro.java
@@ -1,10 +1,21 @@
 package com.bz.pro;
 
-public class DayThreepro {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program");
+public class DayThreepro {
+			double x1,x2,y1,y2;
+			public static void main(String[] args) {
+				System.out.println("Welcome to Line Comparison Computation Program");
+				Scanner sc=new Scanner(System.in);
+			      System.out.println("enter the values:");
+			      double x1=sc.nextDouble();
+			      double x2=sc.nextDouble();
+			      double y1=sc.nextDouble();
+			      double y2=sc.nextDouble();
+				double len=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+				 System.out.println("length is:"+len);
+		}
 
 	}
 
-}
+
